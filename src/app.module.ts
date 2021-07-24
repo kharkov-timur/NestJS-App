8beea61ecfde8e3a265f23c814dbbdbd0780633d
Ivan Shevchenko <rayserviz@gmail.com>
2021-07-24 20:59:18 +0300

    add settings in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { RoleModule } from './user/role/role.module';
@@ -8,11 +8,20 @@ import { configModule } from './configure.root';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      ssl: { rejectUnauthorized: false },
+      entities: [User],
+    }),
     AuthModule,
     UserModule,
     RoleModule,
-    configModule,
   ],
 })
 export class AppModule {}
